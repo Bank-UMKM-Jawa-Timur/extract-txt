658603599d4ef522e6672c5552cef7ec6625af2e
oong26 <mkhalil26122000@gmail.com>
2023-08-24 15:01:09 +0700

    develop : change post process

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import os, json, time, csv
+import os, time, sys
 from flask import *
 from werkzeug.utils import secure_filename
-# from models.a0cy_model import A0CYModel
-# from models.lloan_model import LLOANModel
 from ftplib import FTP
 import requests
+import json as my_json
+import urllib.request
+import urllib
+from urllib.request import urlopen
+import urllib3
+
+# Host
+HOST = '0.0.0.0'
+PORT = 5001
 
 # File requirement
 UPLOAD_FOLDER = 'file_uploads'
@@ -17,10 +24,13 @@ FTP_PASSWORD = "Petromax123."
 FTP_FILES_DIR = "/ftp/upload"
 
 # DWH Host
-DWH_HOST = "https://develop.bankumkm.id/datawarehouse"
+# DWH_HOST = "https://develop.bankumkm.id/datawarehouse"
 DWH_TOKEN = "$2y$10$uK7wv2xbmgOFAWOA./7nn.RMkuDfg4FKy64ad4h0AVqKxEpt0Co2u"
+DWH_HOST = "http://127.0.0.1:8000"
 
 app = Flask(__name__)
+app.config['HOST'] = HOST
+app.config['PORT'] = PORT
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['FTP_HOST'] = FTP_HOST
 app.config['FTP_USER'] = FTP_USER
@@ -29,17 +39,23 @@ app.config['FTP_FILES_DIR'] = FTP_FILES_DIR
 app.config['DWH_HOST'] = DWH_HOST
 app.config['DWH_TOKEN'] = DWH_TOKEN
 
-
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def read_file(path):
-    with open(path, 'r') as in_file:
-        return in_file.readlines()
+def read_file(url):
+    print(f'read file from {url}')
+    data = urllib.request.urlopen(url)
+    result = my_json.load(data)
+    # for line in data:
+    #     result.append(line.decode("ISO-8859-1"))
+    return result
+    # with open(url, 'r',encoding="ISO-8859-1") as in_file:
+    #     return in_file.readlines()
 
 def split_data_with_dictionary(data, dictionary):
     # Split data
+    print('split data')
     result = []
     for i in range(len(data)):
         val = data[i].strip()
@@ -49,69 +65,109 @@ def split_data_with_dictionary(data, dictionary):
             value = value.lstrip().rstrip()
             objects[j['field']] = value
         result.append(objects)
+    print('split data done')
+    
     return result
 
-def get_dictionary(filename):
-    url = "".join([app.config['DWH_HOST'],"/api/v1/dictionary"])
-    params = {'filename': filename}
-    headers =  {"mid-client-key": app.config['DWH_TOKEN']}
+def save_json(data, filename):
+    print("save response to json file")
+    path = f"json/{filename}"
+    # Check whether the specified path exists or not
+    isExist = os.path.exists(path)
+    #printing if the path exists or not
+    if not isExist:
+        # Create a new directory because it does not exist
+        os.makedirs(path)
+        print("The new directory is created!")
 
-    response = requests.get(url,params=params, headers=headers)
+    limit = 1000 # limit per page
+    total_data = data['total']
+    total_page = int(total_data / limit)
+    page = 1
+
+    if (total_data > limit):
+        start_index = 0
+        end_index = limit
+        for page in range(1, total_data):
+            save_file = open(f"{path}/{filename}_{page}.json", "w")
+            start_index = limit * page - limit
+            if (page > 1):
+                end_index = limit * page
+            value = {
+                'total': total_data,
+                'total_per_page': len(data['data'][start_index:end_index]),
+                'data': data['data'][start_index:end_index]
+            }
+            my_json.dump(value, save_file, indent = 6)
+            save_file.close()
+    else:
+        save_file = open(f"{path}/{filename}_{page}.json", "w")
+        value = {
+            'total': total_data,
+            'data': data['data']
+        }
+        my_json.dump(data, save_file, indent = 6)
+        save_file.close()
     
-    return response.json()
+    print("file saved")
 
-@app.route('/', methods=['POST'])
+
+@app.route('/extract', methods=['POST'])
 def post_file():
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        response = {
-            'status': 'failed',
-            'message': 'No file choosen.'
-        }
-        return jsonify(response), 400
-    file = request.files['file']
-    # If the user does not select a file, the browser submits an
-    # empty file without a filename
-    if file.filename == '':
-        response = {
-            'status': 'failed',
-            'message': 'No file choosen.'
-        }
-        return jsonify(response), 400
-    if file and allowed_file(file.filename):
-        # Get uploaded file
-        filename = secure_filename(file.filename)
-        filename_split = filename.split('.')[0]
-        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(path)
+    if request.method == 'POST':
+        try:
+            filename = request.json['file']
+            items = request.json['dictionary']
+            text = read_file(url=request.json['file_url'])
+            # text.remove("")
+            result = text
+            final_result = split_data_with_dictionary(result, items)
 
-        res = get_dictionary(filename_split)
-        items = res['data']['item']
+            final_result = {
+                'total': len(final_result),
+                'data': final_result
+            }
 
-        text = read_file(path=path)
-        text.remove("")
-        result = text
-        final_result = split_data_with_dictionary(result, items)
+            # Save json file
+            save_json(final_result, filename)
 
-        # Connect ftp
-        # ftp = connect_ftp()
+            response = {
+                'status': 'success',
+                'message': 'Successfully upload file.',
+                'filename': filename    
+            }
 
-        response = {
-            'status': 'success',
-            'message': 'Successfully upload file.',
-            'file': text,
-            'data': final_result,
-        }
-        return jsonify(response), 200
+            return jsonify(response), 200
+        except Exception as e:
+            print('error')
+            print(e)
+            response = {
+                'status': 'failed',
+                'message': 'Failed to upload file.',
+                'detail': e
+            }
 
-@app.route('/tes', methods=['GET'])
-def tes():
+            return jsonify(response), 500
+
+@app.route('/json', methods=['GET'])
+def json():
     req = request.args
+    page = int(req['page'])
+    filepath = f"json/{req['filename']}/{req['filename']}_{page}.json"
 
-    filename = req['filename']
-    res = get_dictionary(filename)
+    if (os.path.exists(filepath)):
+        json_file = open(filepath, "r")
+        json = my_json.load(json_file)
+        json_file.close()
+        status = 200
+    else:
+        json = {
+            'status': 'failed',
+            'message': 'File not found'
+        }
+        status = 404
 
-    return jsonify(res), 200
+    return json, status
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host=HOST, debug=True,port=PORT)
